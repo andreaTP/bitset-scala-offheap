69644773067bc7c367efc812abd6dad1c0bed539
Andrea <andrea.peruffo1982@gmail.com>
2015-07-10 18:05:50 +0200

    more

--- src/main/scala/eu/unicredit/offheap/BitSet.scala
+++ src/main/scala/eu/unicredit/offheap/BitSet.scala
@@ -42,12 +42,16 @@ class BitSet(nbits: Int)(implicit a: Allocator) {
   def set(index: Int): Unit =
     set(index, true)
 
-  def set(index: Int, value: Boolean): Unit = {  
+  def set(index: Int, value: Boolean): Unit =  
+    _set(index, _ => value)
+
+  private def _set(index: Int, _value: (Boolean) => Boolean): Unit = {  
     val bytePos = byteOfBitN(index)
     val bitMask = byteMask(index)
     val last = inner(bytePos)
 
     val oldValue = ((last & bitMask) != 0)
+    val value = _value(oldValue)
     if (oldValue != value) {
       if (value)
         inner.update(bytePos, (last | bitMask).toByte)
@@ -56,6 +60,18 @@ class BitSet(nbits: Int)(implicit a: Allocator) {
     }
   }
   
+  
+  def flip(bitIndex: Int): Unit =
+    _set(bitIndex, oldValue => !oldValue)
+
+  //NOT optimized...    
+  def flip(fromIndex: Int, toIndex: Int): Unit =
+    for (i <- fromIndex until toIndex)
+      _set(i, oldValue => !oldValue)
+  
+  def set(fromIndex: Int, toIndex: Int): Unit =
+    set(fromIndex, toIndex, true)
+      
   def set(fromIndex: Int, toIndex: Int, value: Boolean): Unit = {
     val bytesPos =
       (for (i <- fromIndex until toIndex)
@@ -93,13 +109,75 @@ class BitSet(nbits: Int)(implicit a: Allocator) {
     for (i <- bytesPos.zip(values))
       inner.update(i._1, i._2.toByte)
   }
+  
+  def clear(bitIndex: Int): Unit =
+    _set(bitIndex, _ => false)
 
+  def clear(fromIndex: Int, toIndex: Int): Unit =
+    set(fromIndex, toIndex, false)
+    
   def get(index: Int): Boolean = {
     val last = inner(byteOfBitN(index))
 
     (last & byteMask(index)) > 0
   }
+
+  def nextSetBit(fromIndex: Int): Int =
+    _nextBit(fromIndex, x => x)
+
+  def nextClearBit(fromIndex: Int): Int =
+    _nextBit(fromIndex, x => !x)
+
+  def _nextBit(fromIndex: Int, f: Boolean => Boolean): Int = {
+    var res = -1
+    val firstByte = byteOfBitN(fromIndex)
+    for (i <- firstByte until inner.size 
+        if res == -1) {
+      val byte = inner(i)
+      val init = 
+        if (i == firstByte)
+          firstByte % 8
+        else
+          0
+      for (k <- init until 8
+          if res == -1) {
+        if (f((byte & (1 << k)) > 0))
+          res = ((i*8)+k)
+      }
+    }
+    
+    if (res == -1) throw new IndexOutOfBoundsException()
+    else res
+  }
+
+  def previousSetBit(fromIndex: Int): Int =
+    _previousBit(fromIndex, x => x)
+
+  def previousClearBit(fromIndex: Int): Int =
+    _previousBit(fromIndex, x => !x)
   
+  def _previousBit(fromIndex: Int, f: Boolean => Boolean): Int = {
+    var res = -1
+    val firstByte = byteOfBitN(fromIndex)
+    for (i <- (0 until byteOfBitN(fromIndex)).reverse 
+        if res == -1) {
+      val byte = inner(i)
+      val end =
+        if (i == firstByte)
+          8 - (fromIndex % 8)
+        else
+          8
+      for (k <- (0 until end).reverse
+          if res == -1) {
+        if (f((byte & (1 << k)) > 0))
+          res = ((i*8)+k)
+      }
+    }
+    
+    if (res == -1) throw new IndexOutOfBoundsException()
+    else res
+  }
+
   def size(): Int =
     _size
     
--- src/test/scala/eu/unicredit/test/BitSetTest.scala
+++ src/test/scala/eu/unicredit/test/BitSetTest.scala
@@ -109,5 +109,94 @@ class BitSetTest extends FlatSpec {
     jbs.set(4, 20, true)
     assert(bs.toString == jbs.toString)
   }
+  
+  it should "flip bit at specified index" in {
+    val bs = new BitSet()
+    val jbs = new juBitSet()
+    
+    bs.set(0,2,true)
+    
+    jbs.set(0,2,true)
+    
+    bs.flip(1)
+    jbs.flip(1)
+    
+    assert{bs.get(1) == false}
+    assert{bs.get(1) == jbs.get(1)}
+    
+    bs.flip(5)
+    jbs.flip(5)
+    
+    assert{bs.get(5) == true}
+    assert{bs.get(5) == jbs.get(5)}
+  }
+  
+  it should "flip bit in a range" in {
+    val bs = new BitSet()
+    val jbs = new juBitSet()
+    
+    bs.set(0,2,true)
+    
+    jbs.set(0,2,true)
+    
+    bs.flip(1,5)
+    jbs.flip(1,5)
+    
+    assert{bs.get(1) == false}
+    assert{bs.get(1) == jbs.get(1)}
+    assert{bs.get(2) == true}
+    assert{bs.get(2) == jbs.get(2)}
+    assert{bs.get(3) == true}
+    assert{bs.get(3) == jbs.get(3)}
+  }
 
+  it should "get next set bit" in {
+    val bs = new BitSet()
+    val jbs = new juBitSet()
+    
+    bs.set(5)
+    
+    jbs.set(5)
+    
+    assert{bs.nextSetBit(2) == 5}
+    assert{bs.nextSetBit(2) == jbs.nextSetBit(2)}
+  }
+  
+  it should "get next clear bit" in {
+    val bs = new BitSet()
+    val jbs = new juBitSet()
+    
+    bs.set(0,5)
+    
+    jbs.set(0,5)
+    
+    assert{bs.nextClearBit(2) == 5}
+    assert{bs.nextClearBit(2) == jbs.nextClearBit(2)}
+  }
+  
+  it should "get previous set bit" in {
+    val bs = new BitSet()
+    val jbs = new juBitSet()
+    
+    bs.set(5)
+    
+    jbs.set(5)
+    
+    assert{bs.previousSetBit(8) == 5}
+    assert{bs.previousSetBit(8) == jbs.previousSetBit(8)}
+  }
+  
+  /* there is a bug in set to be fixed
+  it should "get previous clear bit" in {
+    val bs = new BitSet()
+    val jbs = new juBitSet()
+    
+    bs.set(6, 10)
+    
+    jbs.set(6, 10)
+    
+    //assert{bs.previousClearBit(8) == 5}
+    assert{bs.previousClearBit(8) == jbs.previousClearBit(8)}
+  }
+  */
 }
